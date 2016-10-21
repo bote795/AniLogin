2324c98412b1fbfbc414f7b0df56eb35b5d475cb
bote795 <nbotell2@gmail.com>
2016-10-21 18:26:21 -0500

    base template for providers

---
+++ src/util/providerBaseClass.js
@@ -0,0 +1,39 @@
+//animeProvider Example
+class AnimeProvider
+{
+    /**
+     * Possible user params
+     * this._username
+     * this._id
+     * this._password (optional)
+     * this._authToken
+     * this._baseAPIURL
+     */
+    constructor()
+        {
+            //example https://anilist.co/api/
+            this._baseAPIURL = "provider";
+        }
+        //username or userid
+        //check what is needed
+    getAnimeList(username = this._user)
+        {
+
+        }
+        //id 
+        //values = {}
+    addAnime(id, values = {})
+        {
+
+        }
+        //id 
+        //values = {}
+    updateAnime(id, values = {})
+    {
+
+    }
+    searchAnimes(name)
+    {
+
+    }
+}
