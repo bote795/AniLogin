2a7264ae60b63fb47bb7ba4fa1ef342e6de85143
bote795 <nbotell2@gmail.com>
2017-03-16 19:33:46 -0700

    added coverall, istanbul, packages for code coverage

--- test/testmyanimelist.js
+++ test/testmyanimelist.js
@@ -2,7 +2,7 @@ var myanimelistProvider = require('./../src/providers/myanimelist');
 const myAnimeListKeys = require('./../src/secrets.js').myAnimeList;
 const debug = require('debug')('myanimelist:test');
 
-const myanimelistclient = new myanimelistProvider(myAnimeListKeys.client._id, myAnimeListKeys.client._secret);
+//const myanimelistclient = new myanimelistProvider(myAnimeListKeys.client._id, myAnimeListKeys.client._secret);
 
 /* debug("authenticate make sure credentials are correct");
  myanimelistclient.authenticate().then(data =>{
@@ -23,13 +23,14 @@ myanimelistclient.searchAnimes("attack on titan").then(data =>{
 });*/
 
 //fixed but issue in npm package
-debug("add Kono Subarashii to watching list")
+/*debug("add Kono Subarashii to watching list")
 myanimelistclient.addAnime(32937,{status: "watching"}).then(data=>{
 	console.log(JSON.stringify(data));
 })
 .catch(err=>{
 	console.log(err);
 });
+*/
 
 
 /*debug("change ep watched number for blue exorcist to ep 1")
